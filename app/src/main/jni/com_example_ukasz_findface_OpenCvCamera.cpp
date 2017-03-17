c2142e4d33ea96c85bfcc386b2eb2185e25d736c
Lukasz <Lukasz9999_12@o2.pl>
2017-03-17 15:20:26 +0100

    poprawka buga w wykrywaniu ruchu po zmianie kamery

--- app/src/main/jni/com_example_ukasz_findface_OpenCvCamera.cpp
+++ app/src/main/jni/com_example_ukasz_findface_OpenCvCamera.cpp
@@ -21,16 +21,15 @@ JNIEXPORT void JNICALL Java_com_example_ukasz_findface_OpenCvCamera_moveDetector
 (JNIEnv *, jclass, jlong camRgba) {
     Mat& image = *(Mat*)camRgba;
 
-
     detectMove(image);
 }
 
 JNIEXPORT void JNICALL Java_com_example_ukasz_findface_OpenCvCamera_moveDetectorBetter
-(JNIEnv *, jclass, jlong camRgba) {
+(JNIEnv *, jclass, jlong camRgba, jboolean changedMode) {
 Mat& image = *(Mat*)camRgba;
 
 
-detectMoveBetter(image);
+detectMoveBetter(image, changedMode);
 }
 
 JNIEXPORT void JNICALL Java_com_example_ukasz_findface_OpenCvCamera_lightDetector
@@ -47,49 +46,12 @@ void blackWhiteMode(Mat& image) {
 
 void detectFace(Mat& frame) {
 
-
-
-//    String face_cascade_name = "haarcascade_frontalface_alt.xml";
-//    String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
-//    CascadeClassifier face_cascade;
-//    CascadeClassifier eyes_cascade;
-//
-//    std::vector<Rect> faces;
-//    Mat frame_gray;
-//
-//    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
-//    equalizeHist( frame_gray, frame_gray );
-//
-//    //-- Detect faces
-//    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
-//
-//    for( size_t i = 0; i < faces.size(); i++ )
-//    {
-//        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-//        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-//
-//        Mat faceROI = frame_gray( faces[i] );
-//        std::vector<Rect> eyes;
-//
-//        //-- In each face, detect eyes
-//        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
-//
-//        for( size_t j = 0; j < eyes.size(); j++ )
-//        {
-//            Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
-//            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-//            circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
-//        }
-//    }
-
-
 }
 
 Mat lastImage; // poprzednia klatka
 bool first = true;
 void detectMove(Mat& image) {
     cvtColor(image, image, COLOR_BGR2GRAY);
-
     if(first) {
         first = false;
         lastImage = image.clone();
@@ -102,9 +64,11 @@ void detectMove(Mat& image) {
 
 bool frst = true;
 Mat firstImage; // pierrwsza klatka
-void detectMoveBetter(Mat& image) {
+void detectMoveBetter(Mat& image, bool mode) {
     cvtColor(image, image, COLOR_BGR2GRAY);
-
+    if(mode) {
+        frst = true;
+    }
     if(frst) {
         frst = false;
         firstImage = image.clone();
